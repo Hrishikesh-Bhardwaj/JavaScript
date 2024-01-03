242010b88c1435477b6a722171d2bf4f4069941f
Hrishikesh Bhardwaj <148894713+Hrishikesh-Bhardwaj@users.noreply.github.com>
2024-01-03 12:32:55 +0000

    conversionOp

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+// let num = 33;
+// console.log("value = ", num);
+// console.log("type = ",typeof(num));
+
+// let num2 = Number("33");
+// console.log("value = ", num2);
+// console.log("type = ",typeof(num2));
+
+// let val =  Number("33abc")
+// console.log("value = ", val);//NaN
+// console.log("type = ",typeof(val)); //number
+
+// let val1 =  Number(true)
+// console.log("value = ", val1); //1
+// console.log("type = ",typeof(val1)); //number
+
+// let val2 =  Number(" ")
+// console.log("value = ", val2); // 0
+// console.log("type = ",typeof(val2)); //number
+
+let val3 =  Number("abc")
+console.log("value = ", val3);
+console.log("type = ",typeof(val3)); 
