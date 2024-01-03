c0afbf7038f497c7845a7bfc91900cbfa46b1e23
Hrishikesh Bhardwaj <148894713+Hrishikesh-Bhardwaj@users.noreply.github.com>
2024-01-03 12:51:42 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,6 +18,23 @@
 // console.log("value = ", val2); // 0
 // console.log("type = ",typeof(val2)); //number
 
-let val3 =  Number("abc")
-console.log("value = ", val3);
-console.log("type = ",typeof(val3)); 
+// let val3 =  Number("abc")
+// console.log("value = ", val3);
+// console.log("type = ",typeof(val3)); 
+
+// ***************************Operations*****************************************
+
+let str1 = "Hrishikesh";
+let str2 = " Bhardwaj";
+console.log(str1 + str2);
+
+
+console.log("1"+ 2);            // 12
+console.log(1+ "2");            // 12
+console.log("1"+ 2 + 2);      //122
+console.log(1+ 2 + "3");     //33  
+
+console.log(+true); //1
+console.log(true+); //error
+
+
