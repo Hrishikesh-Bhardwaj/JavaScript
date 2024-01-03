196f21814506ff65824dd8d095367d0fe5876404
Hrishikesh Bhardwaj <148894713+Hrishikesh-Bhardwaj@users.noreply.github.com>
2024-01-03 14:29:34 +0000

    dataType_summery_updated

---
+++ 01_basics/dataTypes_summery.js
@@ -0,0 +1,16 @@
+// Premitive and Non-premitive
+
+// 1. Primitive 7 types
+// String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// 2. Non-premitive(refrence type)
+// array[], object{}, function
+
+// typeOf operator result: 
+// 1. undefined => "undefined"
+// 2. Null => "object"
+// 3. Boolean => "boolean"
+// 4. Number => "number"
+// 5. String => "String"
+// 6. Object => "object"
+// 7. function => "function"
