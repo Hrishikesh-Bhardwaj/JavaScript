90b3fa153b833de32c00eb3331e02051163aee6e
Hrishikesh Bhardwaj <148894713+Hrishikesh-Bhardwaj@users.noreply.github.com>
2024-01-03 11:28:57 +0000

    varialbles

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144553;
+let accountEmail = "abc@gmail.com";
+var accountPass = "12345";
+accountCity = "Jaipur";
+
+// accountId = 2 //typeError
+console.log(accountID);
+
+//prefer not to use var because of issue in block scope and functional scope
