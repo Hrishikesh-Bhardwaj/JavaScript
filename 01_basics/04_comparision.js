a071cafd792208e2cbd3cd9a9fa7b2c5fd687355
Hrishikesh Bhardwaj <148894713+Hrishikesh-Bhardwaj@users.noreply.github.com>
2024-01-03 13:36:57 +0000

    datatype_summery

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,14 @@
+// console.log("2"===2); //strictly check => check data + datatypes
+// console.log("2"==2); 
+
+// console.log("2">1);
+// console.log("02">1);
+
+// avoid conversions like: 
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0); //null converted into number treated as zero. //True.
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+    
