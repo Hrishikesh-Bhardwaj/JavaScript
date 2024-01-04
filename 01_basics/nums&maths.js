81c33aba07c7400854c7b5f00bed65a0dc34ecbf
Hrishikesh Bhardwaj <148894713+Hrishikesh-Bhardwaj@users.noreply.github.com>
2024-01-04 13:05:54 +0000

    number & maths

---
+++ 01_basics/nums&maths.js
@@ -0,0 +1,54 @@
+// const score = 400;
+// console.log(score);
+// console.log(typeof(score));
+
+
+// const balance =  new Number(400);
+// console.log(balance);
+// console.log(typeof(balance));
+
+// console.log(balance.toString().length);
+// console.log(typeof(balance));
+
+// const otherNum = 123.8966;
+// console.log(otherNum.toPrecision(3))
+
+// const hundreds = 10000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ***************************Math****************************************
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(123.5));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3,9, 2, 1));
+// console.log(Math.max(2, 5, 8, 0));
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
