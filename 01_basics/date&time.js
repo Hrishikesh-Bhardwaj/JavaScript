969bf251066d0dade28350231a79a415767f5ba4
Hrishikesh Bhardwaj <148894713+Hrishikesh-Bhardwaj@users.noreply.github.com>
2024-01-04 16:18:29 +0000

    arrays part 1

---
+++ 01_basics/date&time.js
@@ -0,0 +1,29 @@
+let mydate = new Date();
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.getDate());
+// console.log(mydate.getDay());
+// console.log(mydate.getFullYear());
+
+// console.log(mydate.getMonth());
+// console.log(mydate.toISOString());
+// console.log(mydate.toJSON());
+
+// console.log(Date.now());
+
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+newDate.toLocaleString('default', {
+    weekday: "long"
+})
+console.log(newDate)
+
+
+
+
+
+
+
+
+
---
+++ 02_basics/arrays.js
@@ -0,0 +1,33 @@
+// let arr = [2, 4, 6, 8];
+// console.log(typeof(arr)); //object
+//resizable
+
+// console.log(arr[2]);
+
+
+// let arr2 = new Array(1,2,3,4,5)
+// console.log(arr2);
+// arr2.push(10);
+// arr2.pop()
+// console.log(arr2);
+
+// arr2.unshift(12);  // add element index 0 
+// console.log(arr2);
+
+//arr2.unshift();  // remove element from the index 0th
+//console.log(arr2.includes(25)); //returns false 
+
+// const newArr = myArr.join()//merged and converted into string  
+
+//slice, splice
+const myarr = [0,1,2,3,4,5];
+
+console.log(myarr.slice(1,3)); // [1,2]
+console.log(myarr);  // [0,1,2,3,4,5]
+
+console.log(myarr.splice(1,3)); //[1,2,3] //elements also removed from main array
+console.log(myarr);[0,4,5] 
+
+
+
+
