c7130c5e1e78f3408f91f8da79228b9892393c18
Hrishikesh Bhardwaj <148894713+Hrishikesh-Bhardwaj@users.noreply.github.com>
2024-01-06 16:09:21 +0000

    scope&outputbasedQue

--- 03_basics/scope.js
+++ 03_basics/scope.js
@@ -1,19 +1,41 @@
 if (true) {
     let a = 2;
     const b=3;
-    var c = 4;
+    // var c = 4;
 }
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
 
-if (true) {
-    const username = "hrishikesh";
-    if (username==="hrishikesh") {
-        const website = " google";
-        console.log(username+website);
-    }
-    console.log(website);
-}
-console.log(username);
+// if (true) {
+//     const username = "hrishikesh";
+//     if (username==="hrishikesh") {
+//         const website = " google";
+//         console.log(username+website);
+//     }
+//     console.log(website);
+// }
+// console.log(username);
+
+// console.log([..."ajay"]);
+// let x = {b:1, c:2};
+// let y = Object.keys(x);
+// console.log(y.length);
+
+// let a = 3;
+// console.log(typeof(a));
+
+// let b = new Number(3);
+// console.log(typeof(b));
+
+// let c = 3;
+// console.log(typeof(c));
+
+// console.log(a==b);//true
+// console.log(a===b);//false
+
+// console.log(b==c);//true
+
+
+
