4d4b8aa2534891c50098dc369ddd0b4573b20054
Hrishikesh Bhardwaj <148894713+Hrishikesh-Bhardwaj@users.noreply.github.com>
2024-01-07 16:11:23 +0000

    loopsinJs

---
+++ loops/one.js
@@ -0,0 +1,39 @@
+for (let i = 0; i < 10; i++) {
+    // console.log(i);    
+}
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if (i==5) {
+//         console.log("5 is best number");
+//     }
+//     console.log(element);
+// }
+
+
+// for(i=1;i<=20;i++){
+//     for(j=1;j<=10;j++){
+//         console.log(`${i} * ${j} = ${i*j}`);
+//     }
+//     console.log('\n');
+// }
+
+
+arr = ['flash', 'batman', 'superman', 'spiderman'];
+
+// arr.forEach(elem => {console.log(elem);});
+
+
+//break and continue
+
+// for (let i = 1; i < 10; i++) {
+//     const element = i;
+//     if (i==5) {
+//        break;
+//     //    continue;
+//     }
+//     console.log(element);
+// }
+
+
+
